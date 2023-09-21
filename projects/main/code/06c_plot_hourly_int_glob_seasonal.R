f6314416952356fac2b1154b8e170246e38802cc
Rajani462 <rkpradhan462@gmail.com>
2023-09-21 14:40:03 +0000

    removed redundant

--- projects/main/code/06c_plot_hourly_int_glob_seasonal.R
+++ projects/main/code/06c_plot_hourly_int_glob_seasonal.R
@@ -169,82 +169,6 @@ ggsave("./projects/main/results/06c_24hlineplot_int_landocnglob_seasonal.png",
 
 
 # ### Estimate the peak hour of data.tables -------------------------------------------
-# 
-# #system.time(peak_hour_dt <- data_dt[, .SD[which.max(prec_int)], by = .(lat, lon, name, season)])
-# # user  system elapsed 
-# # 823.215   3.262 694.131 
-# 
-# system.time(peak_hour_list <- lapply(data_list, function(df) {
-#   df[, .SD[which.max(prec_freq)], by = .(lat, lon, name, season)]
-# }))
-# 
-# saveRDS(peak_hour_list, "./projects/main/data/freq_peak_hour_dt_2001_20_seasonal.RDS")
-# 
-# ######
-# 
-# peak_hour_list <- readRDS("./projects/main/data/mean_peak_hour_dt_2001_20_seasonal.RDS")
-# 
-# peak_hour_dt[, `:=`(peak_hour = hour(time_lst))]
-# peak_hour_dt[, `:=`(time_lst = NULL)]
-# # peak_hour_dt[peak_hour  == "1" | peak_hour  == "2" | peak_hour  == "3", peak_hour2 := '1-3']
-# # peak_hour_dt[peak_hour  == "4" | peak_hour  == "5" | peak_hour  == "6", peak_hour2 := '4-6']
-# # peak_hour_dt[peak_hour  == "7" | peak_hour  == "8" | peak_hour  == "9", peak_hour2 := '7-9']
-# # peak_hour_dt[peak_hour  == "10" | peak_hour  == "11" | peak_hour  == "12", peak_hour2 := '10-12']
-# # peak_hour_dt[peak_hour  == "13" | peak_hour  == "14" | peak_hour  == "15", peak_hour2 := '13-15']
-# # peak_hour_dt[peak_hour  == "16" | peak_hour  == "17" | peak_hour  == "18", peak_hour2 := '16-18']
-# # peak_hour_dt[peak_hour  == "19" | peak_hour  == "20" | peak_hour  == "21", peak_hour2 := '19-21']
-# # peak_hour_dt[peak_hour  == "22" | peak_hour  == "23" | peak_hour  == "0", peak_hour2 := '22-00']
-# 
-# peak_hour_dt[peak_hour  == "15" | peak_hour  == "16" | peak_hour  == "17" | peak_hour  == "18", peak_hour2 := '15-18']
-# peak_hour_dt[peak_hour  == "19" | peak_hour  == "20" | peak_hour  == "21" | peak_hour  == "22", peak_hour2 := '19-22']
-# peak_hour_dt[peak_hour  == "23" | peak_hour  == "0" | peak_hour  == "1" | peak_hour  == "2", peak_hour2 := '23-02']
-# peak_hour_dt[peak_hour  == "3" | peak_hour  == "4" | peak_hour  == "5" | peak_hour  == "6", peak_hour2 := '03-06']
-# peak_hour_dt[peak_hour  == "7" | peak_hour  == "8" | peak_hour  == "9" | peak_hour  == "10", peak_hour2 := '07-10']
-# peak_hour_dt[peak_hour  == "11" | peak_hour  == "12" | peak_hour  == "13" | peak_hour  == "14", peak_hour2 := '11-14']
-# 
-# library(forcats)
-# ggplot(peak_hour_dt) + 
-#   geom_raster(aes(lon, lat, fill = factor(peak_hour))) +
-#   scale_fill_manual(values = rainbow(24)) +
-#   borders(colour = "black") +
-#   coord_cartesian(xlim = c(min(peak_hour_dt$lon), max(peak_hour_dt$lon)), 
-#                   ylim = c(min(peak_hour_dt$lat), max(peak_hour_dt$lat))) + 
-#   facet_grid(season~name) +  
-#   scale_x_continuous(expand = c(0, 0)) + 
-#   labs(x = "", y = "", fill = "Peak hour") + 
-#   theme_generic + 
-#   theme(strip.background = element_rect(fill = "white"),
-#         strip.text = element_text(colour = 'Black'), 
-#         legend.direction = "vertical", legend.position = "right", legend.key.width = unit(0.5, "cm"),
-#         legend.key.height = unit(0.9, 'cm'))
-# 
-# ggsave("./projects/main/results/06c_plot_spat_peak_hour_int_seasonal.png", width = 10.5, height = 6.9, 
-#        units = "in", dpi = 600)
-# 
-# ggplot(peak_hour_dt) + 
-#   geom_raster(aes(lon, lat, fill = factor(peak_hour2))) + 
-#   # scale_fill_manual(values = c("#EBB582", "#FFCC66", "#F0810F", "#CC3333",
-#   #                                       "#ACBD78",  "#739F3D", "#99CC00", "#009999")) + 
-#   scale_fill_manual(values = c("#FFCC66", "#9999CC", "#33CCCC", "#99CC00", "#F0810F", "#CC3333")) +
-#   borders(colour = "black") +
-#   coord_cartesian(xlim = c(min(peak_hour_dt$lon), max(peak_hour_dt$lon)), 
-#                   ylim = c(min(peak_hour_dt$lat), max(peak_hour_dt$lat))) + 
-#   facet_grid(season~name) +  
-#   scale_x_continuous(expand = c(0, 0)) + 
-#   labs(x = "", y = "", fill = "Peak hour") + 
-#   theme_generic + 
-#   theme(strip.background = element_rect(fill = "white"),
-#         strip.text = element_text(colour = 'Black'), 
-#         legend.direction = "vertical", legend.position = "right", legend.key.width = unit(0.5, "cm"),
-#         legend.key.height = unit(0.9, 'cm'))
-# 
-# ggsave("./projects/main/results/06c_plot_spat_peak_hour_int_seasonal_clasfy.png", width = 10.5, height = 6.9, 
-#        units = "in", dpi = 600)
-# 
-# ####################################################
-
-# in Robinson projection --------------------------------------------------
-
 
 system.time(peak_hour_list <- lapply(data_list, function(df) {
   df[, .SD[which.max(prec_int)], by = .(lat, lon, name, season)]
@@ -253,6 +177,7 @@ system.time(peak_hour_list <- lapply(data_list, function(df) {
 saveRDS(peak_hour_list, "./projects/main/data/int_peak_hour_dt_2001_20_seasonal.RDS")
 
 ####
+# in Robinson projection -------------------------------------------------
 
 peak_hour_list <- readRDS("./projects/main/data/int_peak_hour_dt_2001_20_seasonal.RDS")
 
@@ -264,12 +189,6 @@ names(data_list) <- c("imerg_jja", "imerg_djf", "gsmap_jja", "gsmap_djf",
                                 "cmorph_jja", "cmorph_djf", "persiann_jja", "persiann_djf", 
                                 "era5_jja", "era5_djf")
 
-
-
-# comb <- rbind(persiann_raw, imerg)
-# comb_uniq <- unique(comb, by = c("lat", "lon"))
-# comb_uniq[name == "IMERG"]
-# comb_uniq[is.na(clusters), name := "PERSIANN"]
 
 #Filling missing pixels (lat, lon) with NA'
 imerg <- data_list$imerg_jja
@@ -361,3 +280,5 @@ ggplot() +
 
 ggsave("./projects/main/results/06c_plot_spat_peak_hour_int_seasonal_robin.png", width = 11.5, height = 5.3, 
        units = "in", dpi = 600)
+
+################################################
