f45820d0097e9ad230088e59f4db06df077d857c
Rajani462 <rkpradhan462@gmail.com>
2023-09-21 11:11:53 +0000

    updated with 0.1  and 0.2 mm/hr threshold

--- projects/main/code/03b_hourly_charact_seasonal_glob.sh
+++ projects/main/code/03b_hourly_charact_seasonal_glob.sh
@@ -26,6 +26,94 @@ done
 
 ##########
 
+## Note: before running please check that all the input datasets have variable name 'precip' and in NetCDF2 format
+
+### Latitude wise zonal mean--------------------------------------------------
+#!/bin/bash
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+# Loop through the input files
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+    # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="lat_mean_${name_parts[0]}_${name_parts[-1]}.nc"
+    
+    
+    # Run the cdo command
+    cdo -b 32 -P 50 -timmean -zonmean "$input_file" "${output_dir}/$output_file"
+done
+-
+
+### seasonal and threshold of 0.1 mm/hr---------
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+# Loop through the input files
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+    # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="lat_mean_${name_parts[0]}_${name_parts[-1]}_0.1.nc"
+    
+    
+    # Run the cdo command
+    cdo -b 32 -P 50 -timmean -zonmean -expr,'precip = (precip >= 0.1 ? precip : 0)' "$input_file" "${output_dir}/$output_file"
+done
+
+
+
+### seasonal and threshold of 0.2 mm/hr---------
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+# Loop through the input files
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+    # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="lat_mean_${name_parts[0]}_${name_parts[-1]}_0.2.nc"
+    
+    
+    # Run the cdo command
+    cdo -b 32 -P 50 -timmean -zonmean -expr,'precip = (precip >= 0.2 ? precip : 0)' "$input_file" "${output_dir}/$output_file"
+done
+
+
+
+### seasonal and threshold of 0.5 mm/hr---------
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+# Loop through the input files
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+    # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="lat_mean_${name_parts[0]}_${name_parts[-1]}_0.5.nc"
+    
+    
+    # Run the cdo command
+    cdo -b 32 -P 50 -timmean -zonmean -expr,'precip = (precip >= 0.5 ? precip : 0)' "$input_file" "${output_dir}/$output_file"
+done
+
 ### Now estimate the diurnal characteritics for each seasonal dataset-----------
 
 ### diurnal mean--------------------------------------------------
@@ -91,4 +179,160 @@ for input_file in "$input_dir"/*.nc; do
     
     # Run the cdo command
     cdo -P 43 -div -dhoursum -mul "$input_file" -expr,'count_value_above_0_1 = (precip >= 0.1 ? 1 : 0)' "$input_file" -dhoursum -expr,'count_value_above_0_1 = (precip >= 0.1 ? 1 : 0)' "$input_file" "${output_dir}/$output_file"
+done
+
+########################################################
+
+
+##  for different season and threshold------------
+
+### diurnal mean--------------------------------------------------
+#!/bin/bash
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+# Loop through the input files
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+    # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="hourly_mean_${name_parts[0]}_${name_parts[-1]}_0.5.nc"
+
+    # Run the cdo command
+        cdo -b 32 -P 50 -dhourmean -expr,'count_value_above_0_5 = (precip >= 0.5 ? precip : 0)' "$input_file" "${output_dir}/$output_file"
+done
+
+# for 0.2 mm/hr
+#!/bin/bash
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+# Loop through the input files
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+    # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="hourly_mean_${name_parts[0]}_${name_parts[-1]}_0.2.nc"
+
+    # Run the cdo command
+        cdo -b 32 -P 50 -dhourmean -expr,'count_value_above_0_2 = (precip >= 0.2 ? precip : 0)' "$input_file" "${output_dir}/$output_file"
+done
+
+
+# for 0.1 mm/hr
+#!/bin/bash
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+# Loop through the input files
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+    # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="hourly_mean_${name_parts[0]}_${name_parts[-1]}_0.1.nc"
+
+    # Run the cdo command
+        cdo -b 32 -P 50 -dhourmean -expr,'count_value_above_0_1 = (precip >= 0.1 ? precip : 0)' "$input_file" "${output_dir}/$output_file"
+done
+
+### diurnal frequency--------------------------------------------------
+
+### for 0.2 mm/hr
+
+#!/bin/bash
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+     # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="hourly_freq_${name_parts[0]}_${name_parts[-1]}_0.2.nc"
+    
+    # Run the cdo command
+    cdo -P 50 -mulc,100 -div -dhoursum -expr,'count_value_above_0_2 = (precip >= 0.2 ? 1 : 0)' "$input_file" -dhoursum -expr,'valid_mask = precip >= 0' "$input_file" "${output_dir}/$output_file"
+done
+
+
+### for 0.5 mm/hr
+
+#!/bin/bash
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+     # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="hourly_freq_${name_parts[0]}_${name_parts[-1]}_0.5.nc"
+    
+    # Run the cdo command
+    cdo -P 50 -mulc,100 -div -dhoursum -expr,'count_value_above_0_5 = (precip >= 0.5 ? 1 : 0)' "$input_file" -dhoursum -expr,'valid_mask = precip >= 0' "$input_file" "${output_dir}/$output_file"
+done
+
+
+
+### diurnal intensity----------------------------------------------------
+
+##for 0.2 mm/hr-----
+
+#!/bin/bash
+cd ~/shared/data_downloads/input_data/seasonal/hourly_character
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+     # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="hourly_int_${name_parts[0]}_${name_parts[-1]}_0.2.nc"
+    
+    # Run the cdo command
+    cdo -P 43 -div -dhoursum -mul "$input_file" -expr,'count_value_above_0_2 = (precip >= 0.2 ? 1 : 0)' "$input_file" -dhoursum -expr,'count_value_above_0_2 = (precip >= 0.2 ? 1 : 0)' "$input_file" "${output_dir}/$output_file" 
+done
+
+
+##for 0.5 mm/hr-----
+
+#!/bin/bash
+cd ~/shared/data_downloads/input_data/seasonal/hourly_character
+
+# Define input and output directories
+input_dir=~/shared/data_projects/diurnal_precip/input_data/seasonal
+output_dir=~/shared/data_projects/diurnal_precip/processed
+
+for input_file in "$input_dir"/*.nc; do
+    # Extract the file name without extension
+    file_name=$(basename "$input_file" .nc)
+    
+     # Extract the relevant parts of the file name
+    IFS="_" read -ra name_parts <<< "$file_name"
+    output_file="hourly_int_${name_parts[0]}_${name_parts[-1]}_0.5.nc"
+    
+    # Run the cdo command
+    cdo -P 43 -div -dhoursum -mul "$input_file" -expr,'count_value_above_0_5 = (precip >= 0.5 ? 1 : 0)' "$input_file" -dhoursum -expr,'count_value_above_0_5 = (precip >= 0.5 ? 1 : 0)' "$input_file" "${output_dir}/$output_file" 
 done
