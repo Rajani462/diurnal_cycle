effbdd9636d492c9255a50e4dbd4170bfb7dc8ab
Rajani462 <rkpradhan462@gmail.com>
2023-10-05 12:48:27 +0000

    era5 without shifting manually 1h backwards

--- projects/02_cdo_preproces_era5.sh
+++ projects/02_cdo_preproces_era5.sh
@@ -32,3 +32,24 @@ cdo -P 50 setrtomiss,5.00e+02,inf ~/shared/data_downloads/PERSIANN/PERSIANN_2001
 
 cdo setrtomiss,-500,-0.0000000001 ~/shared/data_downloads/PERSIANN/PERSIANN_2001_22/persiann_hourly_60ns_2001_2020_regrid_regrid_misrmv.nc ~/shared/data_downloads/PERSIANN/PERSIANN_2001_22/persiann_hourly_60ns_2001_2020_regrid_regrid_misrmv_negt.nc
 #Processed 119442816000 values from 1 variable over 172805 timesteps [25014.94s 230MB]
+
+
+###########################################################################################
+##ERA5 without shifting 1 hour back
+
+cd ~/shared/data_downloads/ERA5
+
+cdo -b F32 -f nc2 -P 40 -mulc,1000 -selyear,2014/2020 adaptor.mars.internal-1665666192.2496276-1964-2-11cbe8c6-8fb2-40c3-974a-7d47fd9ceb2e.nc era5_tp_mm_hourly_2014_2020.nc
+
+cdo -b F32 -f nc2  -P 40 -mulc,1000 adaptor.mars.internal-1665748822.224174-19510-10-fc223fbc-eff1-4323-a9a9-47b325dcda00.nc era5_tp_mm_hourly_2007_2013.nc
+
+cdo -b F32 -f nc2  -P 40 -mulc,1000 adaptor.mars.internal-1665750425.9622443-31349-5-a4a5b123-96e0-4e5e-b07a-6ab5608eb68e.nc era5_tp_mm_hourly_2001_2006.nc
+
+cdo -b F32 -f nc2  -P 40 -chname,tp,precip -remapcon,~/shared/data_downloads/cmorph_merged_hour/cmorph_hour_2001_20_rotated.nc -sellonlatbox,-180,180,-60,60 -mergetime era5_tp_mm_hourly_2001_2006.nc era5_tp_mm_hourly_2007_2013.nc era5_tp_mm_hourly_2014_2020.nc era5_tp_mm_hourly_2001_2020.nc
+#[19726.23s 287MB]
+
+#dhourmean, freq, and intencity
+
+cdo -b F32 -P 40 dhourmean era5_tp_mm_hourly_60ns_025_2001_2020.nc hourly_mean_era5_2001_20.nc #[5655.40s 126MB]
+cdo -b F32 -P 40 -mulc,100 -div -dhoursum -expr,'count_value_above_0_1 = (precip >= 0.1 ? 1 : 0)' era5_tp_mm_hourly_60ns_025_2001_2020.nc -dhoursum -expr,'valid_mask = precip >= 0' era5_tp_mm_hourly_60ns_025_2001_2020.nc hourly_freq_era5_2001_20.nc
+cdo -b F32 -P 40 -div -dhoursum -mul era5_tp_mm_hourly_60ns_025_2001_2020.nc -expr,'count_value_above_0_1 = (precip >= 0.1 ? 1 : 0)' era5_tp_mm_hourly_60ns_025_2001_2020.nc -dhoursum -expr,'count_value_above_0_1 = (precip >= 0.1 ? 1 : 0)' era5_tp_mm_hourly_60ns_025_2001_2020.nc hourly_int_era5_2001_20.nc
